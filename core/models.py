4a6fc7107b9cc0fb7dc04b22dc0756ae33998001
root <root@Aleksandr.localdomain>
2018-04-05 09:47:20 +0300

    В модель добавлен класс контакты

--- core/models.py
+++ core/models.py
@@ -11,3 +11,18 @@ class CoreData(models.Model):
     
     def __str__(self):
         return self.title
+
+class Contacts(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    country = models.CharField(max_length=100, verbose_name='Страна')
+    city = models.CharField(max_length=50, verbose_name='Город')
+    street = models.CharField(max_length=100, verbose_name='Улица')
+    home = models.CharField(max_length=10, verbose_name='Дом')
+    office = models.CharField(max_length=10, verbose_name='Офис')
+    phone_1 = models.CharField(max_length=20, verbose_name='Телефон 1')
+    phone_2 = models.CharField(max_length=20, verbose_name='Телефон 2')
+    mail = models.CharField(max_length=20, verbose_name='email')
+    # soc = 
+
+class SocNet(models.Model):
+    pass
