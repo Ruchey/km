12b9831e9abc99fcc2f514bd32e1af0936967f5a
root <root@Aleksandr.localdomain>
2018-04-06 09:44:06 +0300

    Добавлена модель главного меню

--- core/admin.py
+++ core/admin.py
@@ -10,5 +10,14 @@ class CoreDataAdmin(admin.ModelAdmin):
         (None, {'fields': [('title', 'to_publish', 'on_main'), 'text']}),
     ]
 
+class MainMenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'to_publish', 'is_main', 'usort')
+    list_editable = ('to_publish', 'is_main', 'usort')
+    fieldsets = [
+        (None, {'fields': ['title', 'slug', 'to_publish', 'is_main', 'usort']}),
+    ]
+
+
 admin.site.register(models.CoreData, CoreDataAdmin)
 admin.site.register(models.Contacts)
+admin.site.register(models.MainMenu, MainMenuAdmin)
--- core/models.py
+++ core/models.py
@@ -4,8 +4,27 @@ from django.db import models
 class MainMenu(models.Model):
     title = models.CharField(max_length=50, verbose_name='Заголовок')
     slug = models.SlugField(max_length=50, verbose_name='Метка')
+    to_publish = models.BooleanField(verbose_name='Опубликовать', default=True)
+    is_main = models.BooleanField(verbose_name='Главное', default=False)
     usort = models.PositiveSmallIntegerField(verbose_name='Сортировка', default=1)
-    is_main = models.
+    
+    class Meta:
+        verbose_name = 'Главное меню'
+        verbose_name_plural = 'Главное меню'
+    
+    def __str__(self):
+        return self.title
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.is_main:
+            try:
+                active_menu = MainMenu.objects.get(is_main=True)
+            except:
+                pass
+            else:
+                active_menu.approved = False
+                active_menu.save()
+        super(MainMenu, self).save()
 
 class CoreData(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
