32080ecfad6587d4fb7c3077a178f5a1479e61f6
root <root@Aleksandr.localdomain>
2018-05-01 15:47:02 +0300

    Вывод списка блога и вывод записей блога

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+import pdb
+
+
+class ListView(generic.ListView):
+    model = Post
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts_list'
+    # pdb.set_trace()
+
+    def get_queryset(self):
+        return Post.objects.order_by('-published_date')
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'post_detail'
+    # pdb.set_trace()
+
+    def get_queryset(self):
+        return Post.objects
